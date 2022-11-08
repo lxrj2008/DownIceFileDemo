4c28e1ea212804db929b6853d9279dd5bd0b78d8
梁雄 <lxrj2008@163.com>
2022-11-08 13:09:48 +0800

    方案中新增项目，CEFsharp，解决爬取有cf_clearance安全机制的网站数据

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(537, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "请求接口数据";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(23, 10);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 21);
+            this.dateTimePicker1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-80, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "开始时间";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(261, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "结束时间";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(320, 10);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 21);
+            this.dateTimePicker2.TabIndex = 5;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(520, 636);
+            this.webBrowser1.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(693, 636);
+            this.panel1.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(647, 8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "刷新网页";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.webBrowser1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.panel1);
+            this.splitContainer1.Size = new System.Drawing.Size(1217, 636);
+            this.splitContainer1.SplitterDistance = 520;
+            this.splitContainer1.TabIndex = 11;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.button3);
+            this.splitContainer2.Panel1.Controls.Add(this.button1);
+            this.splitContainer2.Panel1.Controls.Add(this.dateTimePicker1);
+            this.splitContainer2.Panel1.Controls.Add(this.button2);
+            this.splitContainer2.Panel1.Controls.Add(this.label1);
+            this.splitContainer2.Panel1.Controls.Add(this.dateTimePicker2);
+            this.splitContainer2.Panel1.Controls.Add(this.label2);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.splitContainer1);
+            this.splitContainer2.Size = new System.Drawing.Size(1217, 689);
+            this.splitContainer2.SplitterDistance = 49;
+            this.splitContainer2.TabIndex = 12;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(728, 8);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Close";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1217, 689);
+            this.Controls.Add(this.splitContainer2);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Compression;
+using BrotliSharpLib;
+using Newtonsoft.Json;
+using CefSharp;
+using CefSharp.WinForms;
+using System.Threading;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        ChromiumWebBrowser webview = null;
+        string url = "https://cn.investing.com/dividends-calendar/";
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitCEF();
+        }
+        private void InitCEF()
+        {
+            var settings = new CefSettings();
+
+
+            // By default CEF uses an in memory cache, to save cached data e.g. to persist cookies you need to specify a cache path
+            // NOTE: The executing user must have sufficient privileges to write to this folder.
+            settings.CachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CefSharp\\Cache");
+            settings.PersistSessionCookies = true;
+            //settings.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36";
+
+            Cef.Initialize(settings);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webview = new ChromiumWebBrowser();
+            webview.Dock = DockStyle.Fill;
+            this.panel1.Controls.Add(webview);
+            dateTimePicker2.Value = dateTimePicker1.Value.AddDays(7);
+            webview.FrameLoadEnd += WebBrower_FrameLoadEnd;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var url = "https://cn.investing.com/dividends-calendar/Service/getCalendarFilteredData";
+            var paramList = new List<KeyValuePair<string, string>>();
+            paramList.Add(new KeyValuePair<string, string>("currentTab", "custom"));
+            paramList.Add(new KeyValuePair<string, string>("dateFrom", dateTimePicker1.Value.ToString("yyyy-MM-dd")));
+            paramList.Add(new KeyValuePair<string, string>("dateTo", dateTimePicker2.Value.ToString("yyyy-MM-dd")));
+            paramList.Add(new KeyValuePair<string, string>("limit_from", "0"));
+            paramList.Add(new KeyValuePair<string, string>("country[]", "4"));
+            paramList.Add(new KeyValuePair<string, string>("country[]", "5"));
+            paramList.Add(new KeyValuePair<string, string>("country[]", "36"));
+            paramList.Add(new KeyValuePair<string, string>("country[]", "37"));
+            paramList.Add(new KeyValuePair<string, string>("country[]", "39"));
+            var userAgent = "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36";
+            var referer = "https://cn.investing.com/dividends-calendar/";
+            var objdata = RequestPost<GuXiApiData>(url, paramList, userAgent, referer);
+            if (objdata != null)
+            {
+                webBrowser1.DocumentText = "<html><table>" + objdata.data + "</html></table>";
+            }
+
+        }
+        private void WebBrower_FrameLoadEnd(object sender, CefSharp.FrameLoadEndEventArgs e)
+        {
+            //if (e.Frame.IsMain)
+            //{
+            //    SimulationRun();
+            //}
+        }
+        private T RequestPost<T>(string url, List<KeyValuePair<string, string>> paramList, string UserAgent = null, string referer = null)
+        {
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls
+                | SecurityProtocolType.Tls11
+                | SecurityProtocolType.Tls12;
+            ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, errors) => { return true; };
+            var requestUrl = url;
+            var paramStr = "";
+            foreach (var param in paramList)
+            {
+                paramStr += string.Format(@"{0}={1}&", param.Key, param.Value);
+            }
+            paramStr = paramStr.TrimEnd('&');
+            var bytes = Encoding.UTF8.GetBytes(paramStr);
+
+            var request = (HttpWebRequest)WebRequest.Create(requestUrl);
+            request.Method = "POST";
+            request.Timeout = 2 * 60 * 1000;
+            request.Headers["Cookie"] = "udid=4dcba8f7c32514ad0f3e813fb12a95fd;cf_clearance=1260377ad2d8a41a129a6c529eceff790320d141-1666882298-0-150";
+            request.Headers["Accept-Encoding"] = "gzip, deflate, br";
+            request.Headers["Accept-Language"] = "zh-CN,zh;q=0.9";
+            request.Headers["X-Requested-With"] = "XMLHttpRequest";
+            request.ContentType = "application/x-www-form-urlencoded";
+            if (!string.IsNullOrEmpty(UserAgent))
+                request.UserAgent = UserAgent;
+            if (!string.IsNullOrEmpty(referer))
+                request.Referer = referer;
+            request.ContentLength = bytes.Length;
+            var ReadStr = "";
+            try
+            {
+                using (var requestStream = request.GetRequestStream())
+                {
+                    requestStream.Write(bytes, 0, bytes.Length);
+                    requestStream.Close();
+                    using (var reponse = (HttpWebResponse)request.GetResponse())
+                    {
+                        if (reponse.ContentEncoding.ToLower().Contains("gzip"))
+                        {
+                            ReadStr = new StreamReader(new GZipStream(reponse.GetResponseStream(), CompressionMode.Decompress)).ReadToEnd();
+                        }
+                        else if (reponse.ContentEncoding.ToLower().Contains("deflate"))
+                        {
+                            ReadStr = new StreamReader(new DeflateStream(reponse.GetResponseStream(), CompressionMode.Decompress)).ReadToEnd();
+                        }
+                        else if (reponse.ContentEncoding.ToLower().Contains("br"))
+                        {
+                            //需要从NuGet引用 Brotli.Net
+                            ReadStr = new StreamReader(new BrotliStream(reponse.GetResponseStream(), CompressionMode.Decompress)).ReadToEnd();
+                        }
+                        else
+                        {
+                            ReadStr = new StreamReader(reponse.GetResponseStream(), Encoding.UTF8).ReadToEnd();
+
+                        }
+                        var result = JsonConvert.DeserializeObject<T>(ReadStr);
+                        return result;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return default(T);
+        }
+        private bool IsSameMonth()
+        {
+            if(DateTime.Now.Month==DateTime.Now.AddDays(7).Month)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public  async Task<string> GetScriptResult(string javascript)
+        {
+            var result = await webview.EvaluateScriptAsync(javascript);
+            if (result.Success)
+            {
+                return result.Result.ToString();
+            }
+            else
+            {
+                return await webview.GetSourceAsync();
+            }
+        }
+        private void SimulationRun()
+        {
+            webview.GetMainFrame().EvaluateScriptAsync("document.getElementById('datePickerToggleBtn').click();");
+            var task0 = GetScriptResult("function tempFunction0() { var a=$($(\".ui-datepicker-month\")[0]).html();var b=$($(\".ui-datepicker-month\")[1]).html();return a+b;} tempFunction0();");
+            string[] hh=new string[2];
+           
+            hh= task0.Result.Replace("月", ",").TrimEnd(',').Split(',');
+            Thread.Sleep(3000);
+            var CurrentMonth = DateTime.Now.Month.ToString();
+            if(CurrentMonth==hh[0])
+            {
+                if (IsSameMonth())
+                {
+                    webview.GetMainFrame().EvaluateScriptAsync("$(\".ui-datepicker-group.ui-datepicker-group-middle a[target = '_blank']\").each(function(){var val=$(this).text();if(val==\"20\"){$(this).click()}})");
+                }
+                else
+                {
+                    webview.GetMainFrame().EvaluateScriptAsync("$(\".ui-datepicker-group.ui-datepicker-group-last a[target = '_blank']\").each(function(){var val=$(this).text();if(val==\"5\"){$(this).click()}})");
+                }
+            }
+            if(CurrentMonth == hh[1])
+            {
+                webview.GetMainFrame().EvaluateScriptAsync("$(\".ui-datepicker-group.ui-datepicker-group-last a[target = '_blank']\").each(function(){var val=$(this).text();if(val==\"7\"){$(this).click()}})");
+            }
+            Thread.Sleep(2000);
+            webview.GetMainFrame().EvaluateScriptAsync("document.getElementById('applyBtn').click();");
+            int i = 100;
+            DateTime dtime = DateTime.Now;
+            do
+            {
+                webview.GetBrowser().MainFrame.EvaluateScriptAsync("window.scrollTo(0," + i + ")");
+                i += 100;
+                Thread.Sleep(80);
+            } while ((DateTime.Now - dtime).Seconds < 30);
+            webview.GetBrowser().MainFrame.EvaluateScriptAsync("window.scrollTo(0,200)");
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("function tempFunction() {");
+            sb.AppendLine(" return document.getElementById('dividendsCalendarData').outerHTML; ");
+            sb.AppendLine("}");
+            sb.AppendLine("tempFunction();");
+            Thread.Sleep(5000);
+            var task1 = GetScriptResult(sb.ToString());
+            var tablestr = task1.Result;
+            webBrowser1.DocumentText = tablestr;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webview.LoadUrl(url);
+            Thread.Sleep(5000);
+            Task.Run(() =>
+            {
+                SimulationRun();
+            });
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+    public class GuXiApiData
+    {
+        public bool bind_scroll_handler { get; set; }
+        public string last_time_scope { get; set; }
+        public string data{ get;set;}
+        public string dateFrom { get; set; }
+        public string dateTo { get; set; }
+        public int rows_num { get; set; }
+        public string timeframe { get; set; }
+    }
+}
