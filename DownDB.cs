f1977e8cf3df8308f3387a69fe3383e5c502eb92
梁雄 <lxrj2008@163.com>
2022-11-10 10:48:41 +0800

    延时5秒退出

--- DownDB.cs
+++ DownDB.cs
@@ -71,6 +71,7 @@ namespace DemoTest
                 }
             }).ContinueWith(x=>
             {
+                Thread.Sleep(5000);
                 Application.Exit();
             });
         }
